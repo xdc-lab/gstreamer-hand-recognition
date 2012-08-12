34c392c741e27b4b10999821b0ce20575b8e75c6
andol li <yandol@hotmail.com>
2012-08-12 22:35:24 +0100

    updated .gitignore files

--- opencv_handdetect/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_core -lopencv_highgui -lopencv_video -lopencv_imgproc -lopencv_objdetect -lopencv_features2d -lopencv_flann -lopencv_legacy
-
